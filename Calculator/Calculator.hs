01673438132824dbe79fe3a7f993d76b80f1cfcf
Ramin Honary <ramin.honary@gmail.com>
2020-02-14 16:09:24 +0900

    1. Define an Abstract Syntax Tree (AST)

--- Calculator/Calculator.hs
+++ Calculator/Calculator.hs
@@ -1,4 +1,12 @@
 module Main where
 
+data CalcAST
+  = Literal  Double
+  | Label    String
+  | Infix    Char CalcAST CalcAST
+  | Function String CalcAST
+  | Paren    CalcAST
+  deriving (Eq, Read, Show)
+
 main :: IO ()
 main = return ()
