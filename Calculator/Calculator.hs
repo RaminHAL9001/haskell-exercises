281e47dc7e8898e266f6119ffb3e382918d47691
Ramin Honary <ramin.honary@gmail.com>
2020-02-18 15:35:55 +0900

    1.1. Use type synonyms for types imported from other modules

--- Calculator/Calculator.hs
+++ Calculator/Calculator.hs
@@ -1,10 +1,14 @@
 module Main where
 
+type CalcNumber = Double
+type CalcLabel  = String
+type CalcOpCode = Char
+
 data CalcAST
-  = Literal  Double
-  | Label    String
-  | Infix    Char CalcAST CalcAST
-  | Function String CalcAST
+  = Literal  CalcNumber
+  | Label    CalcLabel
+  | Infix    CalcOpCode CalcAST CalcAST
+  | Function CalcLabel  CalcAST
   | Paren    CalcAST
   deriving (Eq, Read, Show)
 
